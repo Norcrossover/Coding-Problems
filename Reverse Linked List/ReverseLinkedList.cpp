2d629943a44c7925dd8d4c97cf56ad203e28a282
Kobe <kobenorcross@gmail.com>
2021-07-13 17:39:13 -0700

    Added pseudo code for iterative approach

--- Reverse Linked List/ReverseLinkedList.cpp
+++ Reverse Linked List/ReverseLinkedList.cpp
@@ -11,6 +11,27 @@
 class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
-        
+        /*          Iterative approach
+        set a counter to find out how many nodes there are by traversing the list
+        go through the list n times
+            for the length of the list traverse to the counter
+                set the current node to head if the counter == 
+
+
+
+                    Recursive approach
+
+        */
+        ListNode* revList = new ListNode; // new reversed list
+        ListNode* counter = head;
+        int count = 0;
+        while (counter) {
+            count++;
+            counter = counter->next;
+        }
+        int max = count;
+        for (int i = 0; i < max; i++) {
+            
+        }
     }
 };
