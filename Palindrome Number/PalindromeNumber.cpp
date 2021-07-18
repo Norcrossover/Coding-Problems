fcc787932159d6d14ba396ef4361e979c80f130c
Kobe <kobenorcross@gmail.com>
2021-07-17 20:09:15 -0700

    Tried to fix y calculation in loop

--- Palindrome Number/PalindromeNumber.cpp
+++ Palindrome Number/PalindromeNumber.cpp
@@ -2,13 +2,27 @@ class Solution {
 public:
     bool isPalindrome(int x) {
         int y = 0; // reversed number
+        /*
+        cout << "x % 9 = " << (x % 9) << endl;
+        cout << "x % 10 = " << (copy % 10) << endl;
+        */
         if (x < 0) x *= -1; // make x positive if it is negative
-        while (x!=0) {
-            y = x%10;
+        int copy = x; 
+        
+        while (x != 0) {
+            cout << "x: " << x << endl;
+            cout << "y: " << y << endl;
+            y = x % 10;
             x /= 10;
             y *= 10;
         }
-        if (x == y) return true;
+        /*
+        cout << "x: " << x << endl;
+        cout << "copy: " << copy << endl;
+        cout << "y: " << y << endl;
+        */
+        if (y < 0) y *= -1;
+        if (copy == y) return true;
         return false;
     }
 };
