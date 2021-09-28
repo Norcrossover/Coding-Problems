dc29e0a1a99a7bbb0fdf9b691dc371270deb5005
Kobe <kobenorcross@gmail.com>
2021-09-28 13:58:59 -0600

    fixed bug that ended program early if x was a negative number: x = -2, n = 2

--- pow(x,n)/pow(x,n).cpp
+++ pow(x,n)/pow(x,n).cpp
@@ -7,21 +7,31 @@ public:
         
         if (n == 0) return 1;
         else if (n > 0) {
-            if (x == 1) return 1; 
-            for (int i=1; i<n; i++) {
-                //cout << solution << endl;
-                if (solution > max || solution < min) return 0;
+            cout << "n > 0" << endl;
+            if (x == 1) {
+                cout << "returned 1";
+                return 1; 
+            }
+            for (int i = 1; i < n; i++) {
+                cout << solution << endl;
+                if (solution > max || (solution < min && solution > 0)) {
+                    cout << "returned 0" << endl;
+                    return 0;
+                }
                 solution *= x;
             }
+            cout << solution << endl;
             return solution;
         }
         else if (n < 0) {
             //n *= -1;
+            cout << "n < 0" << endl;
             solution = 1/x;
-            for (int i=1; i<-n; i++) {
+            for (int i = 1; i < -n; i++) {
                 if (solution < min) return 0;
                 solution /= x;
             }
+            cout << solution << endl;
             return solution;
         };
         return 0;
