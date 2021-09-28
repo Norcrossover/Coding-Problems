e99a4cefd0c73e1aadccaac1ec23820962f5512b
Kobe <kobenorcross@gmail.com>
2021-09-28 12:18:16 -0600

    bug within remove element function that accesses memory wrong

--- Sort Array By Parity 2/main.cpp
+++ Sort Array By Parity 2/main.cpp
@@ -7,23 +7,20 @@ public:
             arr[i] = arr[i+1];
             arr[i+1] = temp;
         }
+        arr.pop_back();
     }
     vector<int> sortArrayByParityII(vector<int>& nums) {
         int len = nums.size();
         vector<int> arr;
-        // loop through arr
         for (int i = 0; i < len; i++) {
-            // loop through nums
             for (int j = 0; j < len; j++) {
-                // if sorted list count is even
                 if (i % 2 == 0 && nums[i] % 2 == 0) {
                     arr.push_back(nums[i]);
-                    nums[i].pop();
+                    removeElement(nums, i);
                 }
-                // else sorted list count is odd
                 else if (i % 2 == 1 && nums[i] % 2 == 1) {
                     arr.push_back(nums[i]);
-                    nums[i].pop();
+                    removeElement(nums, i);
                 }
             }
         }
