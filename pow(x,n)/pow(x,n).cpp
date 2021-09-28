cea6e71b7834e268bdc0595a8ae2fb38b4312ed7
Kobe <kobenorcross@gmail.com>
2021-09-28 14:38:21 -0600

    covered a lot of test cases

--- pow(x,n)/pow(x,n).cpp
+++ pow(x,n)/pow(x,n).cpp
@@ -2,10 +2,12 @@ class Solution {
 public:
     double myPow(double x, int n) {
         double solution = x;
-        double max = 10000;
+        double max = 1000000;
         double min = 1/10000;
         
         if (n == 0) return 1;
+        else if (n > max && x > 1) return 0;
+        else if (x > max) return 0;
         else if (n > 0) {
             cout << "n > 0" << endl;
             if (x == 1) {
@@ -13,7 +15,7 @@ public:
                 return 1; 
             }
             for (int i = 1; i < n; i++) {
-                cout << solution << endl;
+                //cout << solution << endl;
                 if (solution > max || (solution < min && solution > 0)) {
                     cout << "returned 0" << endl;
                     return 0;
