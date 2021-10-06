04d9651c803cacc7981d8ea8b22fc62c063ad5e3
Kobe <kobenorcross@gmail.com>
2021-10-06 16:32:24 -0600

    finished code and going to submit since first test case worked

--- Remove Duplicates from Sorted Array/main.cpp
+++ Remove Duplicates from Sorted Array/main.cpp
@@ -1,6 +1,23 @@
 class Solution {
 public:
+    void removeElement(int val, vector<int>&nums) {
+        int len = nums.size(), temp = 0;
+        for (int i = val; i < nums.size(); i++) {
+            temp = i;
+            nums[i] = nums[i+1];
+            nums[i+1] = temp;
+        }
+        nums.pop_back();
+    }
     int removeDuplicates(vector<int>& nums) {
-        
+        int len = nums.size();
+        for (int i = 0; i < len; i++) {
+            for (int j = i+1; j < len; j++) {
+                if (nums[i] == nums[j]) {
+                    removeElement(j, nums);
+                }
+            }
+        }
+        return nums.size();
     }
 };
