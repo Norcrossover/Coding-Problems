c7d81b98e6fc903337024fc314a0f2faafae4ff6
Norcrossover <59990490+Norcrossover@users.noreply.github.com>
2022-06-08 16:12:37 -0700

    Time: 427 ms (5.32%), Space: 6 MB (32.68%) - LeetHub

---
+++ 9-palindrome-number/9-palindrome-number.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        long long y = 0; // reversed number
+        /*
+        cout << "x % 9 = " << (x % 9) << endl;
+        cout << "x % 10 = " << (copy % 10) << endl;
+        */
+        if (x < 0) return false;//x *= -1; // make x positive if it is negative
+        int copy = x; 
+        
+         while (x) {
+            cout << "x: " << x << endl;
+            cout << "y: " << y << endl;
+            y += x % 10;
+            x /= 10;
+            if (x || y < x /10) y *= 10;
+        }
+        
+        cout << "x: " << x << endl;
+        //cout << "copy: " << copy << endl;
+        cout << "y: " << y << endl;
+        
+        if (y < 0) y *= -1;
+        if (copy == y) return true;
+        return false;
+    }
+};
