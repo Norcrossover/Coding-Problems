4da8c69e86ab8cb8198d174aa417f7b302631464
Norcrossover <59990490+Norcrossover@users.noreply.github.com>
2022-06-12 18:57:12 -0700

    Time: 42 ms (79.71%), Space: 27.6 MB (64.06%) - LeetHub

--- 704-binary-search/704-binary-search.cpp
+++ 704-binary-search/704-binary-search.cpp
@@ -5,9 +5,9 @@ public:
         while (left <= right)
         {
             mid = (left + right) / 2;
-            if (nums[mid] == target) return mid;
-            else if (target < nums[mid]) right = mid-1;
-            else left = mid+1;
+            if (target < nums[mid]) right = mid-1;
+            else if (target > nums[mid]) left = mid+1;
+            else return mid;
         }
         return -1;
     }
